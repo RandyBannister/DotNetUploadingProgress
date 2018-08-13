44726818372a171a2f5d127417f3be788097636b
Michael Albanese <38363373+MichaelAlbanese@users.noreply.github.com>
2018-08-12 20:26:32 -0500

    Add files via upload

---
+++ DotNetUploadingProgress/src/DotNetUploadingProgress/Program.cs
@@ -0,0 +1,25 @@
+﻿// Copyright © 2018 Michael Albanese. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.IO;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+
+namespace AspNetCoreFileUploading
+{
+  public class Program
+  {
+    public static void Main(string[] args)
+    {
+      Program.BuildWebHost(args).Run();
+    }
+
+    public static IWebHost BuildWebHost(string[] args) =>
+      WebHost.CreateDefaultBuilder(args)
+        .UseContentRoot(Directory.GetCurrentDirectory())
+        .UseKestrel()
+        .UseIISIntegration()
+        .UseStartup<Startup>()
+        .Build();
+  }
+}
